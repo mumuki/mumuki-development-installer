1bda923e2298a0b7381d2c7bc20b9ba3f0a2a5bf
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-11-27 12:02:01 -0300

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,19 @@
+Vagrant.configure(2) do |config|
+  config.vm.box = 'ubuntu/trusty64'
+
+  config.vm.box_check_update = false
+  # (`vagrant box outdated` to update)
+
+  (3000..3010).each do |it|
+    config.vm.network 'forwarded_port', guest: it, host: it
+  end
+
+  config.vm.provider 'virtualbox' do |vb|
+    vb.memory = '2048'
+  end
+
+  ssh_public_key = File.readlines(File.join Dir.home, '.ssh', 'id_rsa.pub').first.strip
+  config.vm.provision 'shell', inline: <<-SHELL
+    echo #{ssh_public_key} >> /root/.ssh/authorized_keys
+  SHELL
+end
---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/mumuki/development mumuki
+cd mumuki
+vagrant up
+gem install escualo
+escualo script development.platform.yml --hostname 127.0.0.1 --username root --ssh-port 2222
+
+mkdir runners
+mkdir gems
+
+for component in  atheneum \
+                  desktop \
+                  classroom-api \
+                  bibliotheca;  do
+  git clone https://github.com/mumuki-$component $component
+done
+
+for runner in haskell \
+              prolog \
+              gobstones \
+              javascript \
+              c \
+              cpp \
+              ruby \
+              java \
+              wollok \
+              text \
+              python \
+              elixir \
+              qsim ; do
+  git clone https://github.com/mumuki-$runner-runner runners/$runner
+done
+
+git clone https://github.com/mumukit gems/mumukit
+
+for gem in bridge \
+           auth \
+           inspection \
+           nuntius \
+           core \
+           directives \
+           serice \
+           content-type ; do
+  git clone https://github.com/mumukit-$gem gems/$gem
+done
